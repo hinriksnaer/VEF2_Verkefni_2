bf779ddb6e730d1689df0cc2e6d929d4badf6314
Hinrik <hinriksnaer@gmail.com>
2018-02-16 16:57:18 +0000

    fix

--- schema.sql
+++ schema.sql
@@ -1,8 +1,9 @@
-create table users (
-  id serial primary key,
-  username varchar(64) not null,
-  date timestamp with time zone not null default current_timestamp,
-  email varchar(64) not null,
-  ssn varchar(11) not null,
-  amount INT not null,
-);
+CREATE TABLE public.users
+(
+  id integer NOT NULL DEFAULT nextval('"users_Id_seq"'::regclass),
+  date timestamp with time zone NOT NULL,
+  username text NOT NULL,
+  email text NOT NULL,
+  ssn text NOT NULL,
+  amount integer NOT NULL
+)
