cac35b77e1aa2551758df5e82580533b5a4dcaf5
Hinrik <hinriksnaer@gmail.com>
2018-02-16 16:52:36 +0000

    fix

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
-create table Users (
+create table users (
   id serial primary key,
   username varchar(64) not null,
-  date timestamp with time zone not null,
+  date timestamp with time zone not null default current_timestamp,
   email varchar(64) not null,
   ssn varchar(11) not null,
-  amount integer not null,
+  amount INT not null,
 );
